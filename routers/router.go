bd64225045c6890349e6df07517c12f3d72975ef
dongshanlang <1044052828@qq.com>
2020-05-20 23:12:48 +0800

    test load file

--- routers/router.go
+++ routers/router.go
@@ -1,11 +1,22 @@
 package routers
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
 
-func Init(r *gin.Engine)  {
+func Init(r *gin.Engine) {
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "pong",
 		})
+	})
+	//r.LoadHTMLGlob("views/*")
+	r.LoadHTMLFiles("views/login.html")
+	r.GET("/login", func(ctx *gin.Context) {
+		ctx.HTML(http.StatusOK, "login.html", struct {
+
+		}{})
+
 	})
 }
